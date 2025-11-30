b90425497a00c5b27e7f724c4e685cd0b8cd81e4
yamilss <ya.apablaza@duocuc.cl>
2025-11-30 16:51:59 -0300

    cambio al vite.config
    se cambio ya que al trabajar con github hubieron complicaciones con los head

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: '/Proyecto-Full-Stack-II-React/',
   build: {
     outDir: 'dist',
@@ -19,7 +18,4 @@ export default defineConfig({
     host: true,
     port: 3000,
   },
-=======
-  base: './'
->>>>>>> 3c197a80e905aa7530035257926efb6913fcde1c
 })
